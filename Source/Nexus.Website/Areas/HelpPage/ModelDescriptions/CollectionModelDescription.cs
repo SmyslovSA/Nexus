f362feaf7dc4dde656a814c41d39d22c2b447b29
Stepan Smyslov <smuslov@gmail.com>
2018-07-11 11:36:08 +0300

    + add folders for project + created "fish" solutions

---
+++ Source/Nexus.Website/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Nexus.Website.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Source/Nexus.Website/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Nexus.Website.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
